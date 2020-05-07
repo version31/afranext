b8df0cd4650fe16adc3f03b1d6e3efda11d16577
Ali-Sharifi-Neyestani <sh4rifi@gmail.com>
2020-05-07 23:04:46 +0430

    add newsletter

---
+++ src/sections/newsletter/Newsletter.js
@@ -0,0 +1,41 @@
+import React from "react";
+import {Container, Row, Col} from "react-grid-system";
+import {FaGem, FaCrown, FaSend} from "react-icons/all";
+
+import Heading from "../../components/common/Heading";
+
+function Newsletter(props) {
+    return (
+        <section className="section section-newsletter section-newsletter-1">
+            <div className="display-spacing">
+                <Container>
+                    <Row>
+                        <Col xl={7} >
+                            <Heading
+                                title={["Check your", <br/>, "Website’s SEO!"]}
+                                description="Faff about only a quid blower I don’t want
+no agro bleeding chimney pot burke tosser
+cras nice one boot fanny."/>
+
+
+                            <div className="input-group">
+                                <input type="text" placeholder="Enter URL:"/>
+                                <div className="input-group-append">
+                                    <button className="button button-md button-secondary button-block button-shape">Get
+                                        Started
+                                    </button>
+                                </div>
+                            </div>
+
+                        </Col>
+                        <Col xl={4} >
+                            <img src="/assets/images/phones/iPhoneX-Right.png" alt="alt"/>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        </section>
+    )
+}
+
+export default Newsletter;
