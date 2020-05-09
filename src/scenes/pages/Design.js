6ec2f7e849ab8abd62fd4e0bddc741802cd4941d
Ali-Sharifi-Neyestani <sh4rifi@gmail.com>
2020-05-09 23:58:27 +0430

    add design

---
+++ src/scenes/pages/Design.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import HeaderNext from "../../components/header/HeaderNext";
+import HeroNext from "../../sections/hero/HeroNext";
+import FeaturesNext from "../../sections/features/FeaturesNext";
+import Story from "../../sections/story/Story";
+import Newsletter from "../../sections/newsletter/Newsletter";
+import Blog from "../../sections/blog/Blog";
+import Quote from "../../sections/quote/Quote";
+import StoryDesign from "../../sections/story/StoryDesign";
+import Footer from "../../components/footer/Footer";
+import Process from "../../sections/process/Process";
+
+function Design() {
+    return (
+        <>
+            <HeaderNext/>
+            <StoryDesign/>
+            <Footer/>
+        </>
+    )
+}
+
+export default Design;
